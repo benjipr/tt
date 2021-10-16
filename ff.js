594b79e3d0a03f637acc4265b6165327173dc32d
benjipr <49229894+benjipr@users.noreply.github.com>
2021-10-16 05:54:14 +0200

    Update ff.js

--- ff.js
+++ ff.js
@@ -1,1 +1,7 @@
-require('fs').readdirSync('.').toString()
+var request = require('request');
+var url ='http://requestbin.net/r/fhtk74ne'
+request(url, function (error, response, body) {
+  if (!error) {
+    console.log(body);
+  }
+});
